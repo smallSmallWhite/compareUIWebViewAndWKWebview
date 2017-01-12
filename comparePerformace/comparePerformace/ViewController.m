3767d0e681b86626924b1ce37856173c1893f158
admin <1395258209@qq.com>
2017-01-12 13:56:25 +0800

    第二次提交

--- comparePerformace/comparePerformace/ViewController.m
+++ comparePerformace/comparePerformace/ViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import <WebKit/WebKit.h>
+@interface ViewController ()<UIWebViewDelegate,WKNavigationDelegate>
 
 @end
 
@@ -16,9 +16,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    [self loadWebViewWithUIWebView];
+    [self loadWebViewWithWKWebView];
 }
+#pragma mark  使用UIWebView加载网页
+- (void)loadWebViewWithUIWebView{
+    
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.taobao.com"]]];
+    webView.delegate = self;
+    [self.view addSubview:webView];
+    
+}
+#pragma mark  UIWebView的代理方法
 
+
+
+#pragma mark  使用WKWebView加载网页
+- (void)loadWebViewWithWKWebView{
+    
+    WKWebView *webView = [[WKWebView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.taobao.com"]]];
+    webView.navigationDelegate = self;
+    [self.view addSubview:webView];
+}
+#pragma mark  WKWebView的代理方法
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
